de897c5e2bd1ca2df7933b9395e6b03379d5276f
ZExCloudy <102912993+ZExCloudy@users.noreply.github.com>
2022-12-22 11:00:13 +0530

    Add files via upload

---
+++ DockerDemoApplication.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerDemoApplication {
+	@RequestMapping("/home")
+	public String handler() {
+		return "hello world";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+
+}
